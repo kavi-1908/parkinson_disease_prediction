a03dc8e83d663fd30cb8193aa4d7832dacc92bd1
kavi-1908 <113092821+kavi-1908@users.noreply.github.com>
2023-11-15 19:25:17 +0530

    Add files via upload

---
+++ parkinson_dataset/study.py
@@ -0,0 +1,70 @@
+# 
+# a =[3,9,4,5,7,8,2,2]
+# m=0
+# for i in range(len(n)):
+#     if n[i]> m: 
+#         m=n[i]
+#     else:
+#         continue
+
+# print(m)
+# print(max(n))
+# n = 5
+# # for i in range(n):
+# #     print(" *"*i,end="\n")
+# # for i in range(n):
+# #     print(" *"*(n-i),end="\n")
+# # for i in range(n):
+# #     print("* "*i,end="\n")
+
+# # for i in range(n):
+# #     print("* "*(n-i),end="\n")
+
+
+# for i in range(5):
+#     print("\n")
+#     for j in range(n-i):
+#         print(" *", end="")
+# def fun(n):        
+#     for i in range(n):
+#         count = 0
+#         for j in range(n):
+#             if a[i] +a[j] == sum_need:
+#                 print(a[i],a[j])
+#             if a[i] == sum_need  and  count == 0:
+#                 print(a[i])
+#                 count +=1
+
+# def fun(n):        
+#     for i in range(n):
+#         count = 0
+#         for j in range(n):
+#             for r in range(n):
+#                if a[i] +a[j]+a[r]== sum_need:
+#                    print(a[i],a[j],a[r])
+#                if a[i] == sum_need  and  count == 0:
+#                    print(a[i])
+#                    count +=1
+
+
+        
+# sum_need = 9
+
+# fun(len(a))
+
+
+
+
+# n = 5
+# team = ["a","a","a","b","b"]
+# for i in range(n):
+#     a = input().strip()
+#     team.append(a)
+# print(team)
+# for i in team:
+#     print(team.count(i))
+
+
+a = "monkey"
+for i in range(len(a)):
+    a[i]
---
+++ parkinson_dataset/test.py
@@ -0,0 +1,10 @@
+n =[3,4,5,7,8]
+m=0
+for i in range(len(n)):
+    if n[i]> m: 
+        m=n[i]
+    else:
+        continue
+
+print(m)
+
